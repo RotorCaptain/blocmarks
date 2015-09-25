f42d43771600c6b19939d7b50dc9f6b66b1b6aa3
Chris Steeb <chris.steeb.001@gmail.com>
2015-09-25 20:22:14 +0000

    Fixed topics display and heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20150914195924) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
-    t.integer  "topics_id"
+    t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "bookmarks", ["topics_id"], name: "index_bookmarks_on_topics_id"
+  add_index "bookmarks", ["topic_id"], name: "index_bookmarks_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
